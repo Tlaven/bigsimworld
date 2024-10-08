3c386569373830da34302012ff74486812e5776a
Tlaven <2515406347@qq.com>
2024-10-08 10:29:17 +0800

    t

--- web/src/views/layout/AppBar.vue
+++ web/src/views/layout/AppBar.vue
@@ -1,5 +1,5 @@
 <template>
- <v-app-bar
+  <v-app-bar
     id="app-bar"
     app
     flat
@@ -8,27 +8,29 @@
       <v-btn icon @click="$emit('menu-click')">
         <v-icon>mdi-menu</v-icon>
       </v-btn>
-  
-      <v-toolbar-title class="text-center">
-        My Application
-      </v-toolbar-title>
-  
+
       <v-spacer></v-spacer>
+      <v-toolbar-title class="text-center" style="flex: 1; text-align: center;">
+        <a href="https://github.com/your-username/your-repo" target="_blank" style="color: inherit; text-decoration: none;">
+          BigSimWorld
+        </a>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+
       <RefreshSubscription />
       <v-btn @click="toggleTheme">
         Toggle Theme
       </v-btn>
-    </v-app-bar>
-  </template>
-  
-  <script setup>
-  import { useTheme } from 'vuetify'
-  import RefreshSubscription from '@/components/RefreshSubscription.vue';
-  
-  const theme = useTheme()
-  
-  function toggleTheme() {
-    theme.global.name.value = theme.global.current.value.dark ? 'light' : 'dark'
-  }
-  </script>
-  
+  </v-app-bar>
+</template>
+
+<script setup>
+import { useTheme } from 'vuetify'
+import RefreshSubscription from '@/components/RefreshSubscription.vue';
+
+const theme = useTheme()
+
+function toggleTheme() {
+  theme.global.name.value = theme.global.current.value.dark ? 'light' : 'dark'
+}
+</script>
